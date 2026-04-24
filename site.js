20f97a22ec05a95b234f8ea210dc264c74e42738
Abdu Ibrahim <84140657+ibbe199@users.noreply.github.com>
2026-04-24 08:45:38 +0200

    Add Google Tag Manager container

--- site.js
+++ site.js
@@ -1,3 +1,14 @@
+(function () {
+  var GTM_ID = 'GTM-P96QVPT3';
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+  var firstScript = document.getElementsByTagName('script')[0];
+  var gtmScript = document.createElement('script');
+  gtmScript.async = true;
+  gtmScript.src = 'https://www.googletagmanager.com/gtm.js?id=' + GTM_ID;
+  firstScript.parentNode.insertBefore(gtmScript, firstScript);
+})();
+
 function closeMenu() {
   const nav = document.getElementById('mainNav');
   const button = document.querySelector('.menu-toggle');
